a32a6b669529cdcf6409fd188b98bbaaeb08d836
Kangyun Wang <wky@kangyuns-mbp.home>
2017-07-26 18:00:14 +0200

    Form and api request with axios

--- recipefinder/src/components/App.js
+++ recipefinder/src/components/App.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
+import '../styles/index.css';
+
+import SearchRecipes from './SearchRecipes';
 
 class App extends Component {
 	render() {
 		return (
 			<div>
 				<h2>Recipe Finder</h2>
+				<SearchRecipes />
 			</div>
 		);
 	}
--- recipefinder/src/components/SearchRecipes.js
+++ recipefinder/src/components/SearchRecipes.js
@@ -6,20 +6,58 @@ import {
 	ControlLabel,
 	Button
 } from 'react-bootstrap';
+import axios from 'axios';
 
 class SearchRecipes extends Component {
+	constructor() {
+		super();
+		this.state = {
+			ingredients: '',
+			dish: ''
+		}
+	}
+
+	search() {
+		let { ingredients, dish } = this.state;
+		const url = `http://www.recipepuppy.com/api/?i=${ingredients}&q=${dish}`
+
+		// fetch(url, { method: 'GET' })
+		// .then(res => res.json())
+
+		axios.get(url)
+			.then(res => {
+				console.log(res);
+				return res;
+			})
+			.catch(err => console.log(err))
+	}
+
 	render() {
 		return (
 			<Form inline>
 				<FormGroup>
 					<ControlLabel>Ingredients</ControlLabel>
 					{' '}
-					<FormControl type='text' placeholder='ginger, chicken' />
+					<FormControl
+						type='text'
+						placeholder='e.g. ginger, chicken'
+						onChange={(event) => this.setState({ ingredients: event.target.value })}
+					/>
 				</FormGroup>
 				{' '}
 				<FormGroup>
-
+					<ControlLabel>Dish</ControlLabel>
+					{' '}
+					<FormControl
+						type='text'
+						placeholder='e.g. soup'
+						onChange={(event) => this.setState({ dish: event.target.value })}
+					/>
 				</FormGroup>
+				{' '}
+				<Button
+					onClick={() => this.search()}
+				>Submit</Button>
 			</Form>
 		);
 	}
