12bfdda9b26eddacc5cb72dcbdcc3b7809f70dca
Kangyun Wang <wky@192.168.2.86>
2017-07-28 19:26:41 +0200

    created first action to receive api data

---
+++ meme-generator/src/actions/index.js
@@ -0,0 +1,34 @@
+export const RECEIVE_MEMES = 'RECEIVE_MEMES';
+
+function receiveMemes(json) {
+	let { memes } = json.data;
+	return {
+		type: RECEIVE_MEMES,
+		memes
+	}
+}
+
+function fetchMemesJson() {
+	return fetch('https://api.imgflip.com/get_memes')
+		.then(res => res.json())
+		.catch(err => console.log(err));
+}
+
+export function fetchMemes() {
+	return function (dispatch) {
+		return fetchMemesJson()
+			.then(res => dispatch(receiveMemes(res)))
+			.catch(err => console.log(err));
+	}
+}
+
+// OR
+
+// export function fetchMemes() {
+// 	return function (dispatch) {
+// 		return fetch('https://api.imgflip.com/get_memes')
+// 			.then(res => res.json)
+// 			.then(json => receiveMemes(json))
+// 			.catch(err => console.log(err));
+// 	}
+// }
