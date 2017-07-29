c212a447bedea909089551d6aa44a043bc970eb2
Kangyun Wang <wky@192.168.2.86>
2017-07-29 12:31:57 +0200

    hover animation

--- meme-generator/src/components/MemeItem.js
+++ meme-generator/src/components/MemeItem.js
@@ -1,16 +1,27 @@
 import React, { Component } from 'react';
 
 class MemeItem extends Component {
+	constructor() {
+		super();
+		this.state = {
+			hovered: false
+		}
+	}
+
 	render() {
 		let { meme } = this.props;
 		return (
-			<div className="meme-item">
+			<div
+				className="meme-item"
+				onMouseEnter={() => this.setState({ hovered: true })}
+				onMouseLeave={() => this.setState({ hovered: false })}
+			>
 				<img
 					src={meme.url}
 					alt={meme.name}
-					className="meme-img"
+					className={this.state.hovered ? "meme-img darken-img" : "meme-img"}
 				/>
-				<h4 className="meme-text">{meme.name}</h4>
+				<h4 className={this.state.hovered ? "meme-text" : "no-text"}>{meme.name}</h4>
 			</div>
 		);
 	}
